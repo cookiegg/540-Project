1d2d46bb6baefd7b94182bfcca7acc9436e3ad7e
Steven Spielberg P <stevenspielberg8@gmail.com>
2016-04-26 20:57:49 -0700

    sync

--- trainUandY.py
+++ trainUandY.py
@@ -209,3 +209,37 @@ import matplotlib.pyplot as plt
 toPlot = np.column_stack((U_hat, y_test[:, 0]))
 plt.plot(toPlot)
 plt.show()
+
+#Testing the model with Plant Model
+setpoint=5
+time_steps=1000 #Poits to predict in the future
+utest_start=X_test[0,:,:] #taking first 12 points in the test set to start prediction 
+utest_start= utest_start.reshape((1,12,1))
+nntest_start=NN_test[0]
+nntest_start=nntest_start.reshape(1,3)
+
+
+ykstack=np.zeros(shape=(time_steps,1)) #array which has predicted values
+for i in range(1,time_steps+1): #Predicting next 100 points with lstm and model
+    uhat=model.predict([utest_start, nntest_start], verbose=1)
+    ustack=np.append(utest_start,uhat)
+    ustack=ustack.reshape(1,len(ustack),1)
+    
+    #tacking u[k-3] from ustack
+    uk_3=ustack[0,8,0]
+    yk=nntest_start[0,1]
+    
+    yk1=0.6*yk+1.2*0.05*uk_3
+    ykstack[i-1]=yk1
+    
+ 
+
+    utest1=ustack[0,1:13,0] #updating recent 12 points
+    utest_start=utest1.reshape(1,len(utest1),1) #converting to list of list of list
+    
+    nntest_start=np.array([1,yk1,setpoint])  
+    nntest_start=nntest_start.reshape(1,3)
+
+#plotting points predicted with lstm and model
+plt.plot(ykstack)
+plt.show()
