c4a6b48c2eaded749e4a1961fe85f7bc53ae9865
tingke <tingkekevin@hotmail.com>
2016-04-26 17:29:42 -0700

    added predict function

---
+++ Code/predict.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import csv
+from keras.layers.core import Dense, Activation, Dropout, Merge
+from keras.layers.recurrent import LSTM
+from keras.utils.visualize_util import plot, to_graph
+import copy
+
+def predict_model(model,setpoint,time_steps,utest_start,nntest_start,lstm_length,ykstack):
+    for i in range(1,time_steps+1): #Predicting next 100 points with lstm and model
+        uhat=model.predict([utest_start, nntest_start], verbose=1)
+        ustack=np.append(utest_start,uhat)
+        ustack=ustack.reshape(1,len(ustack),1)
+
+        #tacking u[k-3] from ustack
+        uk_3=ustack[0,(lstm_length-5),0]
+        yk=nntest_start[0,1]
+
+        yk1=0.6*yk+1.2*0.05*uk_3
+        ykstack[i-1]=yk1
+
+        utest1=ustack[0,1:lstm_length,0] #updating recent 12 points
+        utest_start=utest1.reshape(1,len(utest1),1) #converting to list of list of list
+
+        nntest_start=np.array([1,yk1,setpoint])
+        nntest_start=nntest_start.reshape(1,3)
+
+    return ykstack
--- Code/test.py
+++ Code/test.py
@@ -9,6 +9,7 @@ from keras.layers.recurrent import LSTM
 from keras.utils.visualize_util import plot, to_graph
 import copy
 
+import predict as pdt
 import utils as util
 import model as mod
 
@@ -30,7 +31,7 @@ model = util.load_model('./../savedModels/ModelS/model_2', './../savedModels/Mod
 # model.fit([X_train, NN_train], y_train, batch_size=my_batch_size, nb_epoch=my_epoch)
 # print("Training Time : %s seconds --- \n" % (time.time() - start_time))
 
-# # test the model
+# test the model
 U_hat = model.predict([X_test, NN_test], verbose=1)
 U_hat = U_hat.reshape((len(U_hat)))
 loss_and_metrics = model.evaluate([X_test, NN_test], y_test[:, 0])
@@ -41,4 +42,17 @@ toPlot = np.column_stack((U_hat, y_test[:, 0]))
 plt.plot(toPlot)
 plt.show()
 
-# Insert predict code here
+#Testing the model with Plant Model
+setpoint=1
+time_steps=1000 #Poits to predict in the future
+ykstack=np.zeros(shape=(time_steps,1))
+utest_start=X_test[0,:,:] #taking first 12 points in the test set to start prediction
+utest_start= utest_start.reshape((1,lstm_length-1,1))
+nntest_start=NN_test[0]
+nntest_start=nntest_start.reshape(1,3) #3 denotes 3 dimensions
+
+ #array which has predicted values
+ykstack=pdt.predict_model(model,setpoint,time_steps,utest_start,nntest_start,lstm_length,ykstack)
+#plotting points predicted with lstm and model
+plt.plot(ykstack)
+plt.show()
