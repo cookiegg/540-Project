618146bed5d5b1e6bed024420203a32d5f63670f
Steven Spielberg P <stevenspielberg8@gmail.com>
2016-04-27 16:13:31 -0700

    Added prediction function for only nn

--- Code/test_all.py
+++ Code/test_all.py
@@ -43,6 +43,7 @@ plt.plot(toPlot)
 plt.show()
 
 #--------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
 #Testing the model with Plant Model  with LSTM and NN
 
 time_steps=1000 #Poits to predict in the future
@@ -61,18 +62,19 @@ ykstack=pdt.predict_model(model,y_test,time_steps,utest_start,nntest_start,lstm_
 plt.plot(ykstack)
 plt.show()
 
-
+#--------------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------------
 #Testing the model with Plant Model with only LSTM
  #Poits to predict in the future
-setpoint=5
 
-#varying setpoint
+
+#varying setpoint (comment this section if you want to use constant setpoint)
 yreftest=X_test[:,:,2]
 yreftest_comparison=y_test[:,2]
 #varying setpoint
 
 #constant setpoint:
+setpoint=5
 yreftestlen=len(yreftest)
 yreftest=setpoint*np.ones(shape=(yreftestlen,4))
 yreftest_comparison=setpoint*np.ones(shape=(yreftestlen,1))
@@ -90,3 +92,54 @@ plt.plot(yreftest_comparison)
 plt.show()
 
 #--------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------
+#Testing the model with Plant Model with only NN
+
+
+# Get rid of the Y's for targets, keep only the U's
+u_test = y_test[:, 0]
+u_test=u_test.reshape(len(u_test),1)
+utest_start=u_test[0:lstm_length-1]
+
+
+
+#varying setpoint
+#yreftest=NN_test[lstm_length-1:-1,2]
+#yreftest_comparison=yreftest.reshape(len(yreftest),1)
+#varying setpoint
+
+#constant setpoint:
+setpoint=7
+yreftestlen=len(NN_test[lstm_length-1:-1,2])
+yreftest=setpoint*np.ones(shape=(yreftestlen,1))
+yreftest_comparison=yreftest.reshape(yreftestlen,1)
+#constant setpoint
+
+time_step=yreftestlen
+ykstack=np.zeros(shape=(time_steps-1,1))
+
+
+
+NNtest_start=NN_test[lstm_length-2,:]
+NNtest_start=NNtest_start.reshape(len(NNtest_start),1)
+ytest_start=NNtest_start[1]
+yreftest_start=yreftest[0]
+
+bias=1
+NNtest_start=np.column_stack((bias,ytest_start))
+
+NNtest_start=np.column_stack((NNtest_start,yreftest_start))
+
+ykstack=pdt.predict_model_nn(model,lstm_length,time_steps,NNtest_start,utest_start,ytest_start,yreftest_start,yreftest,bias,ykstack)
+
+
+plt.plot(ykstack)
+plt.plot(yreftest_comparison)
+plt.show()
+#------------------------------------------------------------------------------------------
+
+
+
+
+
+
